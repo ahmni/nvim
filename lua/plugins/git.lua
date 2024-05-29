892418e0592e191fbecdce2f9b17fda92add4f2c
Ahmni Pang-Johnson <apangjohnson@netflix.com>
2024-05-29 10:29:15 -0700

    Fix description for gdiff keybindings

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,22 +1,20 @@
 return {
   {
-    'tpope/vim-fugitive',
+    "tpope/vim-fugitive",
     keys = {
       { "<leader>gs", vim.cmd.Git,             desc = "[G]it [S]tatus" },
       { "<leader>gd", "<cmd>Gvdiffsplit!<cr>", desc = "[G]it [D]iff" },
-      { "gf",         "<cmd>diffget //2<cr>",  desc = "[G]it [Status" },
-      { "gh",         "<cmd>diffget //3<cr>",  desc = "[G]it [Status" },
+      { "gf",         "<cmd>diffget //2<cr>",  desc = "[G]it le[F]t diff" },
+      { "gh",         "<cmd>diffget //3<cr>",  desc = "[G]it rig[H]t diff" },
     },
     dependencies = {
-      'tpope/vim-rhubarb',
+      "tpope/vim-rhubarb",
     },
-    cmd = 'G'
+    cmd = "G",
   },
   {
-    event = 'BufRead',
-    'lewis6991/gitsigns.nvim',
-    config = function()
-      require('gitsigns').setup()
-    end
-  }
+    event = "BufRead",
+    "lewis6991/gitsigns.nvim",
+    config = function() require("gitsigns").setup() end,
+  },
 }
