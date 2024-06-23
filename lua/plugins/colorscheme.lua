aeb5b1904850a584e689773044a6bec48ef89925
Ahmni Pang-Johnson <apangjohnson@netflix.com>
2024-06-23 00:37:15 -0700

    Change colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,14 @@
 return {
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function() vim.cmd([[colorscheme tokyonight-night]]) end,
-  },
+	-- {
+	-- 	"folke/tokyonight.nvim",
+	-- 	lazy = false,
+	-- 	priority = 1000,
+	-- 	config = function() vim.cmd([[colorscheme tokyonight-night]]) end,
+	-- },
+	{
+		"EdenEast/nightfox.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function() vim.cmd([[colorscheme duskfox]]) end,
+	},
 }
